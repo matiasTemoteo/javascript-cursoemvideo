571af66859d6e82005aff2125b8c6f0c968ba286
matiasTemoteo <42721290+matiasTemoteo@users.noreply.github.com>
2025-07-21 20:54:06 -0300

    Adding comments for class011

--- class011/ex008.js
+++ class011/ex008.js
@@ -2,6 +2,12 @@ var velocity = 69.5;
 
 console.log(`The velocity is ${velocity} km/h`);
 
+/*
+    Using if to create a conditional estructure, defining the
+    condition and the code block to be executed if the
+    condition is satified with structure 'if'.
+*/
+
 if (velocity > 60) {
     console.log('You passed over the max velocity!');
     console.log('Please Drive always with prudency!');
--- class011/ex009.js
+++ class011/ex009.js
@@ -2,6 +2,12 @@ var country = 'Brazil';
 
 console.log(`You live in ${country}`);
 
+/*
+    Using 'else' structure after condition 'if' structure to
+    define a code block to execute if the condition of 'if'
+    was not satisfied.
+*/
+
 if (country == 'Brazil') {
     console.log('You are Brazilian!');
 } else {
