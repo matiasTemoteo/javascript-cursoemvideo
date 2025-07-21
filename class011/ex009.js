4457ebcdb81d667a62ffbc0c400d44b73dc7ab64
matiasTemoteo <42721290+matiasTemoteo@users.noreply.github.com>
2025-07-21 19:57:13 -0300

    Created file ex009 in class011
    Working with if and else conditions.

---
+++ class011/ex009.js
@@ -0,0 +1,9 @@
+var country = 'Brazil';
+
+console.log(`You live in ${country}`);
+
+if (country == 'Brazil') {
+    console.log('You are Brazilian!');
+} else {
+    console.log('You are Foreign!');
+}
