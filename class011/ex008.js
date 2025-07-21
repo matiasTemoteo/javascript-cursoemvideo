1a09bc889c7e599efe6f7813e7898ffad4133cb0
matiasTemoteo <42721290+matiasTemoteo@users.noreply.github.com>
2025-07-21 19:56:34 -0300

    Created class01 and ex008
    working with conditions, using js file directly in node.js.

---
+++ class011/ex008.js
@@ -0,0 +1,9 @@
+var velocity = 69.5;
+
+console.log(`The velocity is ${velocity} km/h`);
+
+if (velocity > 60) {
+    console.log('You passed over the max velocity!');
+    console.log('Please Drive always with prudency!');
+}
+
