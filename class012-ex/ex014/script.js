c8e710f640c43e84207fb76ae77f2de10a128343
matiasTemoteo <42721290+matiasTemoteo@users.noreply.github.com>
2025-07-22 11:50:33 -0300

    Created ex014 in class012-ex
    Created files for exercise 14.

---
+++ class012-ex/ex014/script.js
@@ -0,0 +1,21 @@
+
+function load () {
+    var date = new Date();
+    var body = window.document.body;
+    var msg = window.document.getElementById('msg');
+    var photo = window.document.getElementById('photo');
+
+    var hours = date.getHours();
+    msg.innerText = `Now, there is ${hours} hrs.`;
+
+    if (hours >= 0 && hours < 12) {
+        photo.src = '../images/morning.png';
+        body.style.backgroundColor = '#e2cd9f';
+    } else if (hours >= 12 && hours < 18) {
+        photo.src = '../images/afternoon.png';
+        body.style.backgroundColor = '#b9846f';
+    } else {
+        photo.src = '../images/night.png';
+        body.style.backgroundColor = '#515154';
+    }
+}
