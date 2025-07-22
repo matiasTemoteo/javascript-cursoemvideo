91a1fd96cd944be047acfcf456409114b47a8d3d
matiasTemoteo <42721290+matiasTemoteo@users.noreply.github.com>
2025-07-22 15:38:34 -0300

    Creating class014 and environment.js file
    Working with 'for loop'.

---
+++ class014/environment.js
@@ -0,0 +1,26 @@
+/*
+var c = 1;
+while(c <= 5) {
+    
+}
+
+do {
+    console.log(c);
+    c++;
+} while(c <= 5);
+
+*/
+
+/*
+    Using structure for to run code in loops.
+    - 'for' - Need declaration of the variable and also
+      declaration of the increment using incremental
+      operators. Also need the condition to when it will
+      runs the code block.
+*/
+
+console.log('Starting...');
+for(var c = 1; c <= 6; c++) {
+    console.log(c);
+}
+console.log('Ending...');
