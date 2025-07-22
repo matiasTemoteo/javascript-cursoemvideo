4e2e205cdca817d29f83148b6d4f44dbac75698b
matiasTemoteo <42721290+matiasTemoteo@users.noreply.github.com>
2025-07-22 13:45:28 -0300

    Created files html,css,js for ex015 in class012
    Created all the files for ex015.

---
+++ class012-ex/ex015/script.js
@@ -0,0 +1,63 @@
+function verify() {
+    var date = new Date();
+    var year = date.getFullYear();
+    var userYear = window.document.getElementById('txtYear');
+    var outputTxt = window.document.getElementById('output-text');
+    var outputImg = window.document.getElementById('output-img');
+
+    if (userYear.value.length == 0 || Number(userYear.value) > year) {
+        window.alert('Please, lookup the data and try again!');
+    } else {
+        var sex = document.getElementsByName('radSex');
+        var img = document.createElement('img');
+        img.setAttribute('id', 'photo');
+        var age = year - Number(userYear.value);
+        var gender = '';
+        if (sex[0].checked) {
+            gender = 'male';
+            if (age >= 0 && age <= 10) {
+                img.setAttribute('src', './images/baby-boy.png');
+            } else if (age > 10 && age <= 25) {
+                img.setAttribute('src', './images/young-boy.png');
+            } else if (age > 25 && age < 65) {
+                img.setAttribute('src', './images/middle-age-man.png');
+            } else {
+                img.setAttribute('src', './images/old-man.png');
+            }
+        } else if (sex[1].checked) {
+            gender = 'female';
+            if (age >= 0 && age <= 10) {
+                img.setAttribute('src', './images/baby-girl.png');
+            } else if (age > 10 && age <= 25) {
+                img.setAttribute('src', './images/young-girl.png');
+            } else if (age > 25 && age < 65) {
+                img.setAttribute('src', './images/middle-age-woman.png');
+            } else {
+                img.setAttribute('src', './images/old-woman.png');
+            }
+        }
+
+        outputTxt.innerHTML = `Age is ${age} as a ${gender}`;
+        outputTxt.style.textAlign = 'center';
+        outputTxt.style.padding = '15px 0px';
+        outputImg.innerHTML = '';
+
+        img.style.width = '240px';
+        outputImg.appendChild(img);
+        outputImg.style.display = 'flex';
+        outputImg.style.justifyContent = 'center';
+        outputImg.style.alignItems = 'center';
+    }
+}
+
+function clearAll() {
+    var userYear = window.document.getElementById('txtYear');
+    var sex = document.getElementsByName('radSex');
+    userYear.value = '';
+    sex[0].checked = true;
+
+    var outputTxt = window.document.getElementById('output-text');
+    var outputImg = window.document.getElementById('output-img');
+    outputTxt.innerHTML = '';
+    outputImg.innerHTML = '';
+}
